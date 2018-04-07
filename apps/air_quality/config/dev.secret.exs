fb7704f1b573d4e93eca7b7bb2f3a68863109fe5
Wojciech Gawronski <afronski@gmail.com>
2018-04-07 09:16:49 +0200

    Secrets.

---
+++ apps/air_quality/config/dev.secret.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :air_quality, :api_keys,
+  airly: "fiAAuS6xtMa55LLpARyWCnT3MDG8OwZc"
---
+++ apps/air_quality_rest_api/config/prod.secret.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :air_quality_rest_api, AirQualityRestAPI.Endpoint,
+  secret_key_base: "CiL07qu/ne7iQqK3yW3a/laNBz7qOA2Ujxmjg3+wu7VXyffGHwv9MgxIm5V2YA7w"
